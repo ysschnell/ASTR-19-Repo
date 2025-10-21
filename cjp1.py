fc5b97198784584b0613600e26669ef13fb706fc
ysschnell <schnellyas@gmail.com>
2025-10-20 20:41:01 -0700

    Code Journal Prompt #1 Solution

---
+++ cjp1.py
@@ -0,0 +1,15 @@
+#Coding Journal Prompt #1: Writing my name, pronouns, favorite movie and favorite food!
+
+def PrintPrompt1():
+	print(f"Full name: Yasmin Schnell")
+	print(f"Pronouns (optional): She/her")
+	print(f"Favorite movie: Bones and All")
+	print(f"Favorite food: Sushi")
+
+#This defines our main() function for our program
+def main():
+	PrintPrompt1()
+
+#When we run the program, this executes first: Python has a failsafe to allow us to write complicated programs
+if __name__=="__main__":
+	main()
